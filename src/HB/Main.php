1117dad37a83409b5dc1c1995fcfaf1f97ddeba0
SavionLegendZzz <saviondeavault@gmail.com>
2015-08-26 17:30:27 -0400

    Added PurePerms Support

--- src/HB/Main.php
+++ src/HB/Main.php
@@ -21,68 +21,12 @@ $this->getServer()->getPluginManager()->registerEvents($this,$this);
 
 public function onJoin(PlayerJoinEvent $ev){
 $p=$ev->getPlayer();
-if($p->getHealth() === 20){
-$p->setNameTag($p->getName()."\n§c100%");
-}
-if($p->getHealth() === 19){
-$p->setNameTag($p->getName()."\n§c95%");
-}
-if($p->getHealth() === 18){
-$p->setNameTag($p->getName()."\n§c90%");
-}
-if($p->getHealth()  === 17){
-$p->setNameTag($p->getName()."\n§c85%");
-}
-if($p->getHealth()  === 16){
-$p->setNameTag($p->getName()."\n§c80%");
-}
-if($p->getHealth()  === 15){
-$p->setNameTag($p->getName()."\n§c75%");
-}
-if($p->getHealth()  === 14){
-$p->setNameTag($p->getName()."\n§c70%");
-}
-if($p->getHealth()  === 13){
-$p->setNameTag($p->getName()."\n§c65%");
-}
-if($p->getHealth()  === 12){
-$p->setNameTag($p->getName()."\n§c60%");
-}
-if($p->getHealth()  === 13){
-$p->setNameTag($p->getName()."\n§c55%");
-}
-if($p->getHealth()  === 12){
-$p->setNameTag($p->getName()."\n§c50%");
-}
-if($p->getHealth()  === 11){
-$p->setNameTag($p->getName()."\n§c45%");
-}
-if($p->getHealth()  === 9){
-$p->setNameTag($p->getName()."\n§c40%");
-}
-if($p->getHealth()  === 8){
-$p->setNameTag($p->getName()."\n§c35%");
-}
-if($p->getHealth()  === 7){
-$p->setNameTag($p->getName()."\n§c30%");
-}
-if($p->getHealth()  === 6){
-$p->setNameTag($p->getName()."\n§c25%");
-}
-if($p->getHealth()  === 5){
-$p->setNameTag($p->getName()."\n§c20%");
-}
-if($p->getHealth()  === 4){
-$p->setNameTag($p->getName()."\n§c15%");
-}
-if($p->getHealth()  === 3){
-$p->setNameTag($p->getName()."\n§c10%");
-}
-if($p->getHealth()  === 2){
-$p->setNameTag($p->getName()."\n§c5%");
-}
-if($p->getHealth()  === 1){
-$p->setNameTag($p->getName()."\n§c1%");
+$group = $this->getServer()->getPluginManager()->getPlugin("PurePerms")->getUser($player)->getGroup();
+		$groupname = $group->getName();
+if($this->getServer()->getPluginManager()->getPlugin("PurePerms") === null){
+$p->setNameTag($p->getName()."\n".($player->getHealth() / $player->getMaxHealth() * 100)."%");
+}else{
+	$p->setNameTag("[".$groupname."]".p->getName()."\n".($player->getHealth() / $player->getMaxHealth() * 100)."%");
 }
 }
 }
