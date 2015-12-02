e12614ba5acaea22dbf4987be0badbaf6ef7c7ab
SavionLegendZzz <saviondeavault@gmail.com>
2015-12-02 18:51:51 -0500

    Update Checker.php

--- src/HB/Checker.php
+++ src/HB/Checker.php
@@ -14,12 +14,14 @@ class Checker extends PluginTask {
 
 	public function onRun($tick){
 	 foreach($this->getOwner()->getServer()->getOnlinePlayers() as $p) {
-	$group = $this->getOwner()->getServer()->getPluginManager()->getPlugin("PurePerms")->getUser($player)->getGroup();
-		$groupname = $group->getName();
+$player = $p;
+$this->PurePerms = $this->getOwner()->getServer()->getPluginManager()->getPlugin("PurePerms");
+        $group = $this->PurePerms->getUserDataMgr($p)->getGroup($p);
+        $groupname = $group->getName();
 if($this->getOwner()->getServer()->getPluginManager()->getPlugin("PurePerms") === null){
 $p->setNameTag($p->getName()."\n".($player->getHealth() / $player->getMaxHealth() * 100)."%");
 }else{
-	$p->setNameTag("[".$groupname."]".$p->getName()."\n".($player->getHealth() / $player->getMaxHealth() * 100)."%");
+	$p->setNameTag("§7[§e".$groupname."§7]§b".$p->getName()."\n§c".($player->getHealth() / $player->getMaxHealth() * 100)."%");
 }
 }
 	 }
